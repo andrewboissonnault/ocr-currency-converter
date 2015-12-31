ca336853d3b523c35d095edd8fa51074b7586664
Andrew Boissonnault <aboissonnault@amstonstudio.com>
2015-12-31 11:51:23 +0700

    Initial Commit

---
+++ CurrencyOCR/NSArray+Map.h
@@ -0,0 +1,16 @@
+//
+//  NSArray+Map.h
+//  BlinkOCR-sample
+//
+//  Created by Andrew Boissonnault on 12/21/15.
+//  Copyright (c) 2015 MicroBlink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Map)
+
+- (NSArray *)mapObjectsUsingBlock:(id (^)(id obj, NSUInteger idx))block;
+- (NSArray *)accumulateObjectsUsingBlock:(NSArray* (^)(id obj, NSUInteger idx))block;
+
+@end
