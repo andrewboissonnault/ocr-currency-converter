def8145a13e5130591232ebe0e2f7768547d135c
Andrew Boissonnault <aboissonnault@amstonstudio.com>
2015-12-31 12:54:35 +0700

    Updated copyrights.

--- CurrencyOCR/AppDelegate.m
+++ CurrencyOCR/AppDelegate.m
@@ -3,7 +3,7 @@
 //  CurrencyOCR
 //
 //  Created by Andrew Boissonnault on 12/20/15.
-//  Copyright (c) 2015 Matchmaker Cafe. All rights reserved.
+//  Copyright (c) 2015 Andrew Boissonnault. All rights reserved.
 //
 
 #import "AppDelegate.h"
--- CurrencyOCR/ViewController.h
+++ CurrencyOCR/ViewController.h
@@ -3,12 +3,16 @@
 //  CurrencyOCR
 //
 //  Created by Andrew Boissonnault on 12/20/15.
-//  Copyright (c) 2015 Matchmaker Cafe. All rights reserved.
+//  Copyright (c) 2015 Andrew Boissonnault. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *labelResult;
+
+- (IBAction)didTapScan:(id)sender;
 
 
 @end
